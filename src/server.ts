ca4fc2baa5dd15ca3df4b9b4dad25403ed1ef3d0
Gabriel Santos <gabriel.santos@bit.com.br>
2022-09-28 08:00:00 -0300

    Configurando o banco

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,8 @@
-import express, { request, response } from "express";
+import "reflect-metadata";
+import express from "express";
+
+import "./database";
 
 const app = express();
 
-/**
- * GET    => Buscar uma informação
- * POST   => Inserir (Criar) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação especifica
-*/
-
-app.get("/test", (request, response) => {
-    // Request  => Entrando
-    // Response => Saindo
-    return response.send("Olá");
-})
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá POST");
-})
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
